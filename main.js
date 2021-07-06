c09a14f3e859785d82999054708c125f52edadc6
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-06 13:25:41 +0300

    creates variables and if statements on sum range

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
 // variables
-var firstCard = ;
-var secondCard = ;
+var firstCard = 8;
+var secondCard = 1;
+var sum = firstCard + secondCard ;
+
+// if statement
+if (sum < 21){
+    console.log("Do you want to draw a new card?");
+} else if (sum === 21){
+    console.log("Great! You have a blackjack!");
+} else if(sum > 21){
+    console.log("You are out of the game");
+} 
+    
