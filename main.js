16b12d45d1d8c25e8a328ac3807a96921d41a1fa
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-06 14:03:28 +0300

    declaring a message on if statements

--- main.js
+++ main.js
@@ -1,14 +1,24 @@
 // variables
-var firstCard = 8;
-var secondCard = 1;
+var firstCard = 56;
+var secondCard = 13;
 var sum = firstCard + secondCard ;
-
-// if statement
-if (sum < 21){
-    console.log("Do you want to draw a new card?");
+var age = 22;
+var hasBlackJack = false;
+var message = "";
+// if statement on sum range
+if (sum <= 20){
+    message ="Do you want to draw a new card?";
 } else if (sum === 21){
-    console.log("Great! You have a blackjack!");
-} else if(sum > 21){
-    console.log("You are out of the game");
+    message = "Great! You have a blackjack!";
+    hasBlackJack = true;
+} else {
+    message = "You are out of the game";
 } 
-    
+// if statement on age range
+if (age <= 21){
+    console.log("You can not enter the club")
+} else {
+    console.log("You are welcome!")
+}
+console.log(message)
+console.log(hasBlackJack);
