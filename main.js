4307227bec72b1c73da8363630b6c89c2e00a706
FredrickWambua <fredrick.musyoki@student.moringaschool.com>
2021-07-07 16:56:55 +0300

    creates newCard function

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 // variables
-var firstCard = 56;
-var secondCard = 13;
+var firstCard = 13;
+var secondCard = 11;
 var sum = firstCard + secondCard ;
 var age = 22;
 var hasBlackJack = false;
 var message = "";
+var messageEl = document.getElementById("message-el");
+var sumEl = document.getElementById("sum-el");
+var cardEl =document.getElementById("cards-el");
+
 
 //start game function
 function startGame(){
+    cardEl.textContent = `Cards: ${firstCard}, ${secondCard}`
+    sumEl.textContent = `Sum : ${sum}`
     // if statement on sum range
 if (sum <= 20){
     message ="Do you want to draw a new card?";
@@ -15,13 +21,23 @@ if (sum <= 20){
     message = "Great! You have a blackjack!";
     hasBlackJack = true;
 } else {
-    message = "You are out of the game";
+    message = "You are out of the game.";
 } 
+
+//message display
+messageEl.textContent = message;
+}
+
+function newCard(){
+    console.log("Drawing a new card from the deck");
+    let card = 8;
+    sum += card;
+    startGame();
+}
+
 // if statement on age range
-if (age <= 21){
-    console.log("You can not enter the club")
-} else {
-    console.log("You are welcome!")
-}
-console.log(message)
-}
+// if (age <= 21){
+//     console.log("You can not enter the club")
+// } else {
+//     console.log("You are welcome!")
+// }
